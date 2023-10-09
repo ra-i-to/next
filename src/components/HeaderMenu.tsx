0660d8b8741cb0b096056934e48ce93e9c26b953
Raito <rightning83@gmail.com>
2023-10-09 12:02:14 +0900

    メニュー項目修正

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -10,70 +10,76 @@ import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { useRouter } from "next/router";
 import { useUser } from "@auth0/nextjs-auth0/client";
-
 import React, { useRef, useState } from "react";
 
 type Props = {
-    handleMenuClose: any;
-    menuOpenFlg: boolean;
+  handleMenuClose: any;
+  menuOpenFlg: boolean;
 };
 
-const menuList = [
+const HeaderMenu = (props: Props) => {
+  const { user, error, isLoading } = useUser();
+
+  const menuList = [
     { text: "ホーム", path: "/home", icon: HomeIcon, loggedin: true },
     {
-        text: "プロフィール",
-        path: "/profile",
-        icon: AccountCircleIcon,
-        loggedin: true,
+      text: "プロフィール",
+      path: "/profile",
+      icon: AccountCircleIcon,
+      loggedin: true,
     },
     {
-        text: "ログイン",
-        path: "/api/auth/login",
-        icon: LoginIcon,
-        loggedin: false,
+      text: "ログイン",
+      path: "/api/auth/login",
+      icon: LoginIcon,
+      loggedin: false,
     },
+    // {
+    //     text: "新規登録",
+    //     path: "/api/auth/login",
+    //     icon: LoginIcon,
+    //     loggedin: false,
+    //     onClick: auth0Redirect
+    // },
     {
-        text: "新規登録",
-        path: "/api/auth/login",
-        icon: LoginIcon,
-        loggedin: false,
+      text: "ログアウト",
+      path: "/api/auth/logout",
+      icon: LogoutIcon,
+      loggedin: true,
     },
-    {
-        text: "ログアウト",
-        path: "/api/auth/logout",
-        icon: LogoutIcon,
-        loggedin: true,
-    },
-];
+  ];
 
-const HeaderMenu = (props: Props) => {
-    const { user, error, isLoading } = useUser();
-
-    const router = useRouter();
-    return (
-        <>
-            <Drawer anchor="left" open={props.menuOpenFlg} onClose={props.handleMenuClose}>
-                <List>
-                    {menuList.map((menu, index) => {
-                        if ((menu.loggedin && user) || (!menu.loggedin && !user)) {
-                            return (
-                                <ListItem key={index} disablePadding>
-                                    <ListItemButton
-                                        onClick={() => {
-                                            router.push(menu.path);
-                                        }}
-                                    >
-                                        <ListItemIcon>{React.createElement(menu.icon)}</ListItemIcon>
-                                        <ListItemText primary={menu.text} />
-                                    </ListItemButton>
-                                </ListItem>
-                            );
-                        }
-                    })}
-                </List>
-            </Drawer>
-        </>
-    );
+  const router = useRouter();
+  return (
+    <>
+      <Drawer
+        anchor="left"
+        open={props.menuOpenFlg}
+        onClose={props.handleMenuClose}
+      >
+        <List>
+          {menuList.map((menu, index) => {
+            if ((menu.loggedin && user) || (!menu.loggedin && !user)) {
+              return (
+                <ListItem key={index} disablePadding>
+                  <ListItemButton
+                    onClick={() => {
+                      router.push(menu.path);
+                    }}
+                  >
+                    <ListItemIcon>
+                      {React.createElement(menu.icon)}
+                    </ListItemIcon>
+                    <ListItemText primary={menu.text} />
+                  </ListItemButton>
+                </ListItem>
+              );
+            }
+          })}
+        </List>
+      </Drawer>
+    </>
+  );
 };
 
 export default HeaderMenu;
